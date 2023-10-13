2519647569b43a0d7d661613631a7e712ce824fd
Kur <mateusz.kur@wbdcontractor.com>
2023-10-13 16:15:06 +0200

    t1

--- frontend/src/components/Admin/NavBar/AdminNavBar.js
+++ frontend/src/components/Admin/NavBar/AdminNavBar.js
@@ -1,20 +1,21 @@
 import styles from './AdminNavBar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiceD6, faUsers, faListUl, faFlag } from '@fortawesome/free-solid-svg-icons';
+import {Link} from "react-router-dom";
 
 export const AdminNavBar = props => {
     return (
         <>
             <header className={styles.header}>
                 <h3 className={styles.titleAdmin}>
-                    <span className={styles.titleAdminSpan}>c</span> CODERZ <span className={styles.titleAdminSpan}>/></span>
+                    <Link to={'/'}><span className={styles.titleAdminSpan}>c</span> CODERZ <span className={styles.titleAdminSpan}>/></span></Link>
                 </h3>
                 <nav>
                     <ul className={styles.ul}>
-                        <li className={styles.ulLi}><a href="#"><FontAwesomeIcon icon={faDiceD6} className={`${styles.faSolid} ${styles.faDiceD6}`} /> Dashboard</a></li>
-                        <li className={styles.ulLi}><a href="#"><FontAwesomeIcon icon={faUsers} className={`${styles.navUsers} ${styles.faSolid} ${styles.faUsers}`} /> Manage users</a></li>
-                        <li className={styles.ulLi}><a href="#"><FontAwesomeIcon icon={faListUl} className={`${styles.faSolid} ${styles.faListUl}`} /> Manage thread</a></li>
-                        <li className={styles.ulLi}><a href="#"><FontAwesomeIcon icon={faFlag} className={`${styles.faSolid} ${styles.faFlag}`} /> Report system</a></li>
+                        <li className={styles.ulLi}><Link to="/admin/dashboard"><FontAwesomeIcon icon={faDiceD6} className={`${styles.faSolid} ${styles.faDiceD6}`} /> Dashboard</Link></li>
+                        <li className={styles.ulLi}><Link to="/admin/manage-users"><FontAwesomeIcon icon={faUsers} className={`${styles.navUsers} ${styles.faSolid} ${styles.faUsers}`} /> Manage users</Link></li>
+                        <li className={styles.ulLi}><Link to="/admin/manage-thread"><FontAwesomeIcon icon={faListUl} className={`${styles.faSolid} ${styles.faListUl}`} /> Manage thread</Link></li>
+                        <li className={styles.ulLi}><Link to="/admin/report-system"><FontAwesomeIcon icon={faFlag} className={`${styles.faSolid} ${styles.faFlag}`} /> Report system</Link></li>
                     </ul>
                 </nav>
             </header>
