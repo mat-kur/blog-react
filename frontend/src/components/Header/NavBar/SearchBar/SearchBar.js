c9f8e016f36a52fdde81cd5844650d48b0008928
Kur <mateusz.kur@wbdcontractor.com>
2023-10-20 12:04:59 +0200

    test21

--- frontend/src/components/Header/NavBar/SearchBar/SearchBar.js
+++ frontend/src/components/Header/NavBar/SearchBar/SearchBar.js
@@ -2,37 +2,17 @@ import {useState} from "react";
 
 
 export const SearchBar = ({searchQuery, setSearchQuery, threads, setThreads}) => {
-    //
-    // setSearchQuery('test')
-    //
-    // console.log(searchQuery)
+
     const [query, setQuery] = useState('');
-
-    // console.log(searchQuery)
     const handleSearch = async () => {
-        setSearchQuery('test')
-
-        console.log(searchQuery)
-        // if (typeof setSearchQuery === "function") {
-        //     setSearchQuery(query);
-        //     const URL = `http://localhost:5000/api/homepage?q=test`;
-        //     const response = await fetch(URL);
-        //     const data = await response.json();
-        //     setThreads(data);
-        // } else {
-        //     console.error("setSearchQuery is not a function");
-        // }
+        console.log(query)
+        setSearchQuery(query);
+        const URL = `http://localhost:5000/api/homepage?q=test`;
+        const response = await fetch(URL);
+        const data = await response.json();
+        setThreads(data);
     };
 
-    // const handleSearch = async () => {
-    //     console.log(query);
-    //     if (typeof setSearchQuery === "function") {
-    //         setSearchQuery(query);
-    //     } else {
-    //         console.error("setSearchQuery is not a function");
-    //     }
-    //     // reszta twojego kodu
-    // };
 
 
     return (
